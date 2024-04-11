5c29ffb0aaeeb53eef2418dd908aecd60af21941
“Tiago <tiago881@gmail.com>
2024-04-11 10:01:33 -0300

    feat(product):get product details

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,9 +1,39 @@
+import { PRODUCTS } from "@/utils/data/products";
+import { formatCurrency } from "@/utils/functions/format-currency";
 import { useLocalSearchParams } from "expo-router";
-import { View } from "react-native";
+import { Image, View, Text } from "react-native";
 
 export default function Product() {
   const { id } = useLocalSearchParams();
-  console.log(id);
 
-  return <View className="flex-1"></View>;
+  const product = PRODUCTS.filter((item) => item.id === id)[0];
+
+  return (
+    <View className="flex-1">
+      <Image
+        source={product.cover}
+        className="w-full h-52 "
+        resizeMode="cover"
+      />
+
+      <View className="p-5 mt-8 flex-1">
+        <Text className="text-lime-400 text-2xl font-heading my-2">
+          {formatCurrency(product.price)}
+        </Text>
+
+        <Text className="text-slate-400 font-body text-base leading-6 mb-6">
+          {product.description}
+        </Text>
+
+        {product.ingredients.map((ingredient) => (
+          <Text
+            key={ingredient}
+            className="text-slate-400 font-body text-base leading-6"
+          >
+            {"\u2022"} {ingredient}
+          </Text>
+        ))}
+      </View>
+    </View>
+  );
 }
