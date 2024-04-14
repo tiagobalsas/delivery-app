a5fa03b777dd9b8d0b65f0760a21f3b908f81e87
Tiago Santos <tiago881@gmail.com>
2024-04-14 14:54:44 -0300

    fix(SectionList):add ProductProps type

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -7,14 +7,14 @@ import { useCartStore } from "@/stores/cart-store";
 import { CategoryButton } from "@/components/category-button";
 import Header from "@/components/header";
 
-import { CATEGORIES, MENU } from "@/utils/data/products";
+import { CATEGORIES, MENU, ProductProps } from "@/utils/data/products";
 import { Product } from "@/components/product";
 
 export default function Home() {
   const cartStore = useCartStore();
 
   const [category, setCategory] = useState(CATEGORIES[0]);
-  const sectionListref = useRef<SectionList>(null);
+  const sectionListref = useRef<SectionList<ProductProps>>(null);
 
   const cartQuantityItems = cartStore.products.reduce(
     (total, product) => total + product.quantity,
