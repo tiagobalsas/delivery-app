9c0951aff45168421fdaa8ca7babdf40dc16d328
“Tiago <tiago881@gmail.com>
2024-04-14 17:06:54 -0300

    feat(cart):add address input

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,9 +1,11 @@
 import { ScrollView, Text, View } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 import { useCartStore } from "@/stores/cart-store";
 import { formatCurrency } from "@/utils/functions/format-currency";
 
 import Header from "@/components/header";
+import Input from "@/components/input";
 import { Product } from "@/components/product";
 import { LinkButton } from "@/components/link-button";
 
@@ -18,25 +20,37 @@ export default function Cart() {
   return (
     <View className="flex-1 pt-8">
       <Header title="Seu carrinho" />
-      <ScrollView>
-        {cartStore.products.length > 0 ? (
-          <View className="flex-1 p-5">
-            {cartStore.products.map((product) => (
-              <Product key={product.id} data={product} />
-            ))}
-          </View>
-        ) : (
-          <Text className="text-slate-400 text-center font-body my-8">
-            Seu carrinho está vazio.
-          </Text>
-        )}
 
-        <View className="flex-row items-center gap-2 mt-5 mb-4 ">
-          <Text className="text-white text-xl font-subtitle">Total:</Text>
-          <Text className="text-lime-400 text-2xl font-heading">{total}</Text>
+      <KeyboardAwareScrollView
+        showsVerticalScrollIndicator={false}
+        extraHeight={100}
+      >
+        <View className="flex-1 p-5">
+          <ScrollView>
+            {cartStore.products.length > 0 ? (
+              <View className="border-b border-slate-700">
+                {cartStore.products.map((product) => (
+                  <Product key={product.id} data={product} />
+                ))}
+              </View>
+            ) : (
+              <Text className="text-slate-400 text-center font-body my-8">
+                Seu carrinho está vazio.
+              </Text>
+            )}
+
+            <View className="flex-row items-center gap-2 mt-5 mb-4 ">
+              <Text className="text-white text-xl font-subtitle">Total:</Text>
+
+              <Text className="text-lime-400 text-2xl font-heading">
+                {total}
+              </Text>
+            </View>
+
+            <Input placeholder="Informe o endereço de entrega com rua, bairro, CEP, número e complemento..." />
+          </ScrollView>
         </View>
-      </ScrollView>
-      <LinkButton href="/" title="Voltar"></LinkButton>
+      </KeyboardAwareScrollView>
     </View>
   );
 }
