854c1e3d1322c7634a820f48c94a16ec8d551dc1
Tiago Santos <tiago881@gmail.com>
2024-04-14 20:42:29 -0300

    feat(cart):add persisting data

--- src/stores/cart-store.ts
+++ src/stores/cart-store.ts
@@ -1,6 +1,8 @@
-import { ProductProps } from "@/utils/data/products";
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
+import { ProductProps } from "@/utils/data/products";
 import * as cartInMemory from "./helpers/cart-in-memory";
 
 export type ProductCartProps = ProductProps & {
@@ -13,16 +15,24 @@ export type StateProps = {
   remove: (product: string) => void;
 };
 
-export const useCartStore = create<StateProps>((set) => ({
-  products: [],
+export const useCartStore = create(
+  persist<StateProps>(
+    (set) => ({
+      products: [],
 
-  add: (product: ProductProps) =>
-    set((state) => ({
-      products: cartInMemory.add(state.products, product),
-    })),
+      add: (product: ProductProps) =>
+        set((state) => ({
+          products: cartInMemory.add(state.products, product),
+        })),
 
-  remove: (productId: string) =>
-    set((state) => ({
-      products: cartInMemory.remove(state.products, productId),
-    })),
-}));
+      remove: (productId: string) =>
+        set((state) => ({
+          products: cartInMemory.remove(state.products, productId),
+        })),
+    }),
+    {
+      name: "delivery-app",
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
