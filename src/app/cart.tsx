a9dc2427f68a2199bab249748bd77913008ad021
“Tiago <tiago881@gmail.com>
2024-04-14 16:23:12 -0300

    feat(cart):add total products value

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,6 +1,7 @@
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 
 import { useCartStore } from "@/stores/cart-store";
+import { formatCurrency } from "@/utils/functions/format-currency";
 
 import Header from "@/components/header";
 import { Product } from "@/components/product";
@@ -8,20 +9,33 @@ import { LinkButton } from "@/components/link-button";
 
 export default function Cart() {
   const cartStore = useCartStore();
+  const total = formatCurrency(
+    cartStore.products.reduce(
+      (total, product) => total + product.price * product.quantity,
+      0
+    )
+  );
   return (
     <View className="flex-1 pt-8">
       <Header title="Seu carrinho" />
-      {cartStore.products.length > 0 ? (
-        <View className="flex-1 p-5">
-          {cartStore.products.map((product) => (
-            <Product key={product.id} data={product} />
-          ))}
+      <ScrollView>
+        {cartStore.products.length > 0 ? (
+          <View className="flex-1 p-5">
+            {cartStore.products.map((product) => (
+              <Product key={product.id} data={product} />
+            ))}
+          </View>
+        ) : (
+          <Text className="text-slate-400 text-center font-body my-8">
+            Seu carrinho está vazio.
+          </Text>
+        )}
+
+        <View className="flex-row items-center gap-2 mt-5 mb-4 ">
+          <Text className="text-white text-xl font-subtitle">Total:</Text>
+          <Text className="text-lime-400 text-2xl font-heading">{total}</Text>
         </View>
-      ) : (
-        <Text className="text-slate-400 text-center font-body my-8">
-          Seu carrinho está vazio.
-        </Text>
-      )}
+      </ScrollView>
       <LinkButton href="/" title="Voltar"></LinkButton>
     </View>
   );
